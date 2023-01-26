e0f4323e50f9e0f122c7279eacc884013a546a13
Tianle Chen <tianlechen@gmail.com>
2023-01-26 16:50:00 -0500

    fix and move constant

--- tests/sfm/types_test.py
+++ tests/sfm/types_test.py
@@ -1,6 +1,30 @@
-from sfm.types import parse_raw_managed_dirs
+from sfm.types import Group, ManagedDir, parse_raw_managed_dirs
 from tests import TEST_RAW_CONFIG
-from tests.sfm import TEST_PARSED_MANAGED_DIRS
+
+TEST_PARSED_MANAGED_DIRS = {
+    'managed_dir_A': ManagedDir(
+        base_dir='tests/example_directory_a',
+        groups={
+            'group_A': Group(
+                path='group_A_name',
+                keywords=['keyword_A', 'keyword_B'],
+            ),
+            'group_C': Group(
+                path='some_subdir/group_C',
+                keywords=['keyword_C']
+            ),
+        }
+    ),
+    'managed_dir_B': ManagedDir(
+        base_dir='tests/example_directory_b',
+        groups={
+            'group_A': Group(
+                path='group_A_name',
+                keywords=['keyword_A', 'keyword_B'],
+            )
+        }
+    )
+}
 
 
 def test_parse_raw_managed_dirs():
