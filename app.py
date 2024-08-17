e8e9ee9fc4f8c37db5f6f1a4e94d4c761a515fa1
Tianle Chen <tianlechen@gmail.com>
2024-08-17 23:54:34 +0000

    decorate main

--- app.py
+++ app.py
@@ -1,8 +1,17 @@
 import streamlit as st
 
+HOME_INTRO = """
+# Simple Folder Management
+
+This app helps you manage your files and directories using keywords as move triggers.
+"""
+
+with st.sidebar:
+    st.markdown(HOME_INTRO)
+
 pg = st.navigation(
     pages=[
-        st.Page("views/pending.py", title="Pending"),
+        st.Page("views/pending.py", title="Pending Moves"),
         st.Page("views/list.py", title="List Existing Groups"),
         st.Page("views/create.py", title="Create New Group"),
     ]
