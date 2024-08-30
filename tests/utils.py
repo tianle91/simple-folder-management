e590d2302b3ee80a6ebb5a4e31c85336a2a98905
Tianle Chen <tianlechen@gmail.com>
2024-08-30 12:59:29 +0000

    linting

--- tests/utils.py
+++ tests/utils.py
@@ -1,5 +1,6 @@
-from typing import List
 import os
+from typing import List
+
 
 def make_files_at_dir(dir: str, paths: List[str]) -> None:
     if not os.path.exists(dir):
@@ -7,5 +8,5 @@ def make_files_at_dir(dir: str, paths: List[str]) -> None:
     for p in paths:
         absolute_p = os.path.join(dir, p)
         os.makedirs(os.path.dirname(absolute_p), exist_ok=True)
-        with open(absolute_p, 'w') as f:
-            f.write('')
+        with open(absolute_p, "w") as f:
+            f.write("")
