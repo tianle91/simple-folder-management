728cb3a98122fc852bb30846d9cad7b3497b1185
Tianle Chen <tianlechen@gmail.com>
2024-09-01 12:05:09 +0000

    use tabs to separate files and dirs

--- views/list.py
+++ views/list.py
@@ -11,5 +11,21 @@ Here are the existing groups that are set up to automatically move files or dire
 
 st.markdown(LIST_INTRO)
 groups = get_groups()
-for group in groups.values():
-    render_group(group)
+
+file_groups = {}
+dir_groups = {}
+for k, g in groups.items():
+    if g.move_files:
+        file_groups[k] = g
+    else:
+        dir_groups[k] = g
+
+file_tab, dir_tab = st.tabs(
+    [f"File Groups ({len(file_groups)})", f"Directory Groups ({len(dir_groups)})"]
+)
+with file_tab:
+    for group in file_groups.values():
+        render_group(group)
+with dir_tab:
+    for group in dir_groups.values():
+        render_group(group)
